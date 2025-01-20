3f754b3aa372a7edceb69eebbbb94ef6b3ef4728
Mohammed Khalaf <mohamedkhalaf20172020@gmail.com>
2025-01-20 03:45:58 +0200

    FIRE FIGHTING ROBOT PROJECT

---
+++ Fire Fighting Robot/Fire Fighting Robot/main.c
@@ -0,0 +1,16 @@
+/*
+ * Fire Fighting Robot.c
+ *
+ * Created: 4/28/2023 8:43:32 PM
+ * Author : Mohammed Khalaf
+ */ 
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
